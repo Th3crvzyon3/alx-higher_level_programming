2e7e0f6a7b5e8c82f9104fa946ae6bfe62a52134
Abraham Otieno <abrahamlincoln8212gmail.com>
2022-05-30 21:39:07 +0300

    Advanced tasks

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-file.write("and that piece of art is useful - Dora Korpar, 2015-10-19")
+import sys
+str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(str)
 sys.exit(1)
