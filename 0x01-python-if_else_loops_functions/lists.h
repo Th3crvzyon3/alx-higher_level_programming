f13be6d1c5222076c9fa3905fd0b519e5dcb3c58
Abraham Otieno <abrahamlincoln8212gmail.com>
2022-05-31 22:00:35 +0300

    C again??

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,4 +1,4 @@
-ifndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 /**
@@ -11,9 +11,11 @@ ifndef LISTS_H
  */
 typedef struct listint_s
 {
-  int n;
-  struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
+
+#include <stdlib.h>
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
