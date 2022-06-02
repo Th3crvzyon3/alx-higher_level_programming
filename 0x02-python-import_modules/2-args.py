a101b430290c94293331b2589f9ca37541f93d2d
Abraham Otieno <abrahamlincoln8212gmail.com>
2022-06-02 22:11:24 +0300

    Print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
+import sys
 
 if __name__ == "__main__":
-    import sys
-    print_arg(sys.argv)
+    """Print all given argu"""
+
+    numOfArg = len(sys.argv)
+    if (numOfArg == 1):
+        print("0 arguments.")
+    elif numOfArg == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(numOfArg - 1))
+        for i in range(1, numOfArg):
+            print("{}: {}".format(i, sys.argv[i]))
