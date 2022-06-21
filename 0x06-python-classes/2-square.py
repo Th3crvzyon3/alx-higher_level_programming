20681f7aee75eeafbd6a948634c31f4bba774d00
Abraham Otieno <abrahamlincoln8212gmail.com>
2022-06-21 20:36:43 +0300

    Python OOP

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """ A Square class """
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
